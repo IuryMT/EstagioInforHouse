15b466622ec87b4b3628ad5eb54bfdb3fa18e736
iuryteno23 <iurytenorio2005@gmail.com>
2022-09-12 09:45:18 -0300

    alterações

--- src/App.js
+++ src/App.js
@@ -7,11 +7,44 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Home } from './pages/Home/index.js';
 import { Login } from './pages/Login/index.js';
 import { Main } from './components/Main.js';
+// import { Evento } from './components/Evento.js';
 
 
 
 // import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
+
+  // function MobileNavbar() {
+    
+  //   Constructor(mobileMenu, navList, navLinks){  
+  //     super(mobileMenu, navList, navLinks);   
+  //     this.mobileMenu = document.querySelector(mobileMenu);
+  //     this.navList = document.querySelector(navList);
+  //     this.navLinks = document.querySelectorAll(navLinks);
+  //     this.activeClass = "active";
+  //   };
+
+  //   function addClickEvent(){
+  //     this.mobileMenu.addEventListener("click", () => {console.log("iury aqui")});
+  //   }
+
+  //   function init() {
+  //     if(this.mobileMenu) {
+  //       this.addClickEvent();
+  //     }
+  //     return this;
+  //   }
+
+  // }
+
+  // const mobileNavbar = new MobileNavbar(
+  //   ".mobile-menu",
+  //   ".navList",
+  //   ".navList li",
+  // ); 
+
+  // mobileNavbar.init();
+
   return (
     <div className="App">
       <header className='header'>
@@ -19,6 +52,11 @@ function App() {
           <nav className='nav'>
             {/* <img className='img' src={logo192} alt="not found"/> */}
             <h1 className='logo'>Infor House</h1>
+            <div className='mobile-menu'>
+              <div className='line-1'></div>
+              <div className='line-2'></div>
+              <div className='line-3'></div>
+            </div>
             <ul className='navList'>
               <li>
                 <Link to="/">Home</Link>
