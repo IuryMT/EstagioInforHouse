407123d54c9968ec53681aa74aceb23ab5ce423d
iuryteno23 <iurytenorio2005@gmail.com>
2022-09-12 21:13:02 -0300

    alteração de carousel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,18 +6,16 @@ import mulher from "../../img/mulher.png";
 import { Carousel } from 'antd';
 // import 'antd/dist/antd.css';
 import 'antd/dist/antd.min.css'
-import { ShoppingCartOutlined, TagOutlined, UserOutlined, HomeOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons'
+import { ShoppingCartOutlined, TagOutlined, UserOutlined, HomeOutlined } from '@ant-design/icons'
 
 
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css"
-import "swiper/components/navigation/navigation.min.css"
-
-import "./styles.css";
+import "../../../node_modules/swiper/swiper.min.css";
+// import "../../../node_modules/swiper/components/pagination/pagination.min.css";
+// import "../../../node_modules/swiper/components/navigation/navigation.min.css";
 
 
 // import Swiper core and required modules
@@ -35,48 +33,31 @@ export const Home = () => {
 
     const [swiperRef, setSwiperRef] = useState(null);
 
-  let appendNumber = 4;
-  let prependNumber = 1;
+//   let appendNumber = 4;
+//   let prependNumber = 1;
 
-  const prepend2 = () => {
-    swiperRef.prependSlide([
-      '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
-      '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
-    ]);
-  }
+//   const prepend2 = () => {
+//     swiperRef.prependSlide([
+//       '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>',
+//       '<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>'
+//     ]);
+//   }
 
-  const prepend = () => {
-    swiperRef.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
-  }
+//   const prepend = () => {
+//     swiperRef.prependSlide('<div class="swiper-slide">Slide ' + (--prependNumber) + '</div>');
+//   }
 
-  const append = () => {
-    swiperRef.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
-  }
+//   const append = () => {
+//     swiperRef.appendSlide('<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>');
+//   }
 
-  const append2 = () => {
-    swiperRef.appendSlide([
-      '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
-      '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
-    ]);
-  }
+//   const append2 = () => {
+//     swiperRef.appendSlide([
+//       '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>',
+//       '<div class="swiper-slide">Slide ' + (++appendNumber) + '</div>'
+//     ]);
+//   }
 
-
-    // function sliderDireita() {
-    //     const aaa = document.querySelector(".carousel-master");
-    //     document.querySelector(".botao-right").addEventListener("click", () => {
-    //         console.log(aaa);
-    //         aaa.scroll(+150, 0);
-    //         const aaa = null;
-    // })
-    // }
-
-    // function sliderEsquerda() {
-    //     const aaa = document.querySelector(".carousel-master");
-    //     document.querySelector("#esquerda").addEventListener("click", event => {
-    //         console.log(aaa);
-    //         aaa.scroll(150, 0);
-    //     })
-    // }
 
     return (
         <>
@@ -89,9 +70,9 @@ export const Home = () => {
             <div className='englobar'>
                 <h1 className='h1'>Categorias em Destaque</h1>
                 <div className="carousel-master">
-                    <button className="botao-left" id="esquerda" >
+                    {/* <button className="botao-left" id="esquerda" >
                     <LeftOutlined style={{ fontSize: '40px', color: '#ef0505', margin: '5px' }} />
-                    </button>
+                    </button> */}
                     <div className='card'>
                         <img src={relogio} className='imagem' alt="not found" />
                         <h4>Carregadores</h4>
@@ -108,9 +89,9 @@ export const Home = () => {
                         <img src={relogio} className='imagem' alt="not found" />
                         <h4>Caixinhas</h4>
                     </div>
-                    <button className="botao-right" >
+                    {/* <button className="botao-right" >
                     <RightOutlined style={{ fontSize: '40px', color: '#ef0505', margin: '5px' }} />
-                    </button>
+                    </button> */}
                 </div>
             </div>
             <div className='englobar'>
@@ -144,26 +125,38 @@ export const Home = () => {
 
 
 
-
-<Swiper onSwiper={setSwiperRef} slidesPerView={3} centeredSlides={true} spaceBetween={30} pagination={{
+<div className='carousel-master2'>
+    <Swiper onSwiper={setSwiperRef} slidesPerView={3} centeredSlides={true} spaceBetween={30} pagination={{
   "type": "fraction"
 }} navigation={true} className="mySwiper">
-    <SwiperSlide>Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
+    
+        <SwiperSlide>
+            <div className='card'>
+                <img src={relogio} className='imagem' alt="not found" />
+                <h4>Carregadores</h4>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='card2'>
+                <img src={relogio} className='imagem' alt="not found" />
+                <h2>Capinhas</h2>
+                <h3>Descrição</h3>
+            </div>
+        </SwiperSlide>
+    <SwiperSlide><img src={som} className='imagem' alt="not found" /></SwiperSlide>
+    <SwiperSlide><img src={som} className='imagem' alt="not found" /></SwiperSlide>
   </Swiper>
-
-  <p className="append-buttons">
+</div>
+  {/* <p className="append-buttons"> */}
   
 
   
   
-  <button onClick={() => prepend2()} className="prepend-2-slides">Prepend 2 Slides</button>
+  {/* <button onClick={() => prepend2()} className="prepend-2-slides">Prepend 2 Slides</button>
   <button onClick={() => prepend()} className="prepend-slide">Prepend Slide</button>
   <button onClick={() => append()} className="append-slide">Append Slide</button>
-  <button onClick={() => append2()} className="append-2-slides">Append 2 Slides</button>
-  </p>
+  <button onClick={() => append2()} className="append-2-slides">Append 2 Slides</button> */}
+  {/* </p> */}
 
 
 
