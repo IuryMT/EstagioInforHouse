9c73a6e8d1bf47507024b0ee1e3bcf44362ce2e8
MarioRan19 <mariorodriguescruz@gmail.com>
2022-09-20 10:31:43 -0300

    deixei o login mobale e o main/vendas tambem, além da transparência no menu mobile

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,47 +5,47 @@ import mer from '../../img/mer.jpg'
 
 export const Login = () => {
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ email, password });
-    }
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+}
 
-    return (
-        <>
-            <div className='content'>
-                <img src={mer} alt="bfuyv" className='bgf' />
-                <form onSubmit={handleSubmit} className="Formulario">
-                    <h1>Login</h1>
-                    <div id="field">
-                        <label htmlFor='email'>Email: </label>
-                        <input
-                            className='input'
-                            type='email'
-                            name='email'
-                            id='email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div id="field">
-                        <label htmlFor='password'>Senha</label>
-                        <input
-                            className='input'
-                            type='password'
-                            name='password'
-                            id='password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div id='actions'>
-                        <button type='submit' className='enviar'>Enviar</button>
-                    </div>
-                </form>
+return (
+    <>
+    <div className='content'>
+        <img src={mer} alt="bfuyv" className='bgf' />
+        <form onSubmit={handleSubmit} className="Formulario">
+            <h1>Login</h1>
+            <div id="field">
+                <label htmlFor='email'>Email: </label>
+                <input
+                    className='input'
+                    type='email'
+                    name='email'
+                    id='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
-        </>
+            <div id="field">
+                <label htmlFor='password'>Senha</label>
+                <input
+                    className='input'
+                    type='password'
+                    name='password'
+                    id='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div id='actions'>
+                <button type='submit' className='enviar'>Enviar</button>
+            </div>
+        </form>
+    </div>
+    </>
     );
 }
