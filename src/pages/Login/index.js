c336c5e82838b76a007625861108838a48579d68
MarioRan19 <mariorodriguescruz@gmail.com>
2022-09-27 15:44:34 -0300

    Alterei

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,50 +2,64 @@ import '../../components/css/Login.scss';
 import { useState } from 'react';
 import React from 'react'
 import mer from '../../img/mer.jpg'
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 export const Login = () => {
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({ email, password });
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ email, password });
+    }
 
-return (
-    <>
-    <div className='content'>
-        <img src={mer} alt="bfuyv" className='bgf' />
-        <form onSubmit={handleSubmit} className="Formulario">
-            <h1>Login</h1>
-            <div id="field">
-                <label htmlFor='email'>Email: </label>
-                <input
-                    className='input'
-                    type='email'
-                    name='email'
-                    id='email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-            </div>
-            <div id="field">
-                <label htmlFor='password'>Senha</label>
-                <input
-                    className='input'
-                    type='password'
-                    name='password'
-                    id='password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </div>
-            <div id='actions'>
-                <button type='submit' className='enviar'>Enviar</button>
-            </div>
-        </form>
-    </div>
-    </>
-    );
-}
+
+            return (
+                <>
+
+                    <Layout>
+                        <Header>Header</Header>
+                        <Layout>
+                            <Content>
+                                <div className='content'>
+                                    <img src={mer} alt="bfuyv" className='bgf' />
+                                    <form onSubmit={handleSubmit} className="Formulario">
+                                        <h1>Login</h1>
+                                        <div id="field">
+                                            <label htmlFor='email'>Email: </label>
+                                            <input
+                                                className='input'
+                                                type='email'
+                                                name='email'
+                                                id='email'
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            />
+                                        </div>
+                                        <div id="field">
+                                            <label htmlFor='password'>Senha</label>
+                                            <input
+                                                className='input'
+                                                type='password'
+                                                name='password'
+                                                id='password'
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                        </div>
+                                        <div id='actions'>
+                                            <button type='submit' className='enviar'>Enviar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </Content>
+
+                        </Layout>
+                    </Layout>
+
+
+                </>
+            );
+        }
